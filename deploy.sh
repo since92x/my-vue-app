f3833200573262f490bf834ef1fea07aca25b048
m <since92x@gmail.com>
2019-10-11 18:10:35 +0800

    improve script

--- deploy.sh
+++ deploy.sh
@@ -7,4 +7,5 @@ git init
 git add -A
 git commit -m 'deploy'
 git push -f git@github.com:since92x/since92x.github.io.git master
+# git push -f  http://username:password@github.com/dog996/dog996.github.io.git master
 cd -
